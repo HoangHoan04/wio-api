9d7732ba205fe89d8cdb4e7d0275512e804edbc4
HoangHoan <hoanghoanpineapple04@gmail.com>
2026-07-13 13:33:09 +0700

    first commit

--- src/modules/giphy/giphy.module.ts
+++ src/modules/giphy/giphy.module.ts
@@ -1,8 +1,8 @@
-import { HttpModule } from "@nestjs/axios";
-import { Module } from "@nestjs/common";
-import { GiphyUserController } from "./controllers/giphy-user.controller";
-import { GiphyService } from "./giphy.service";
-import { PexelsService } from "./pexels.service";
+import { HttpModule } from '@nestjs/axios';
+import { Module } from '@nestjs/common';
+import { GiphyUserController } from './controllers/giphy-user.controller';
+import { GiphyService } from './giphy.service';
+import { PexelsService } from './pexels.service';
 
 @Module({
   imports: [HttpModule],
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,4 +2,3 @@ export * from './admin';
 export * from './giphy/giphy.module';
 export * from './upload-file';
 export * from './user';
-
